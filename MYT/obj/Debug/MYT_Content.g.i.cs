93a7ec6682c9dfc6e7a99b667916cf6d15b730a9
Marina <mrtihova@gmail.com>
2017-12-07 12:29:28 +0200

    Replace the entire thing in the name of design :bless:

--- MYT/obj/Debug/MYT_Content.g.i.cs
+++ MYT/obj/Debug/MYT_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("myt_icon.ico")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/4.png")]
 
 
